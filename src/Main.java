6e5541d41fb7d667bc4d31d5c7bb91d3b1e680d9
talktoeac <talktoeac@gmail.com>
2022-09-18 06:33:44 +0200

    Changes repository git, merch code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
 
-
     public static void main(String[] args) {
-        suma(10, 20, 30);
+        addFunction(10, 20, 30);
+        Coche miCoche = new Coche();
+        miCoche.addDoor();
+        System.out.println("Total Doors " + miCoche.doors);
     }
 
-    public static void suma(int a, int b, int c) {
-        int resultado = a + b + c;
-
-        System.out.println(resultado);
+    public static void addFunction(int a, int b, int c) {
+        int result = a + b + c;
+        System.out.println("Result " + result);
     }
-
 }
+class Coche{
+    public int doors = 4;
 
-
+    public void addDoor(){
+        this.doors++;
+    }
+}
